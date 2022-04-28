bd75d35c828cd64a6820cca022b88a82a518a52b
phoenix377 <phoenix.number377@gmail.com>
2022-04-27 17:50:18 -0700

    feat: paywall section

---
+++ src/components/layouts/main/blocks/PayWall.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="bg-white shadow paywall-section">
+    <img src="@/assets/img/platt-plus.png" />
+    <div>
+      <h2>Get Started Today</h2>
+      <h2 class="sub-title">
+        <span>Become a </span>
+        <span class="green">Platt Plus </span>
+        <span>Member</span>
+      </h2>
+      <button>Learn More</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class PayWall extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.paywall-section {
+  margin: 1rem 0.5rem;
+  padding: 2rem 1rem;
+  text-align: center;
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin: 1rem 0rem;
+  }
+
+  h2 {
+    margin: 0;
+    &.sub-title {
+      margin: 0;
+      padding-bottom: 0.5rem;
+    }
+
+    span.green {
+      color: var(--green);
+    }
+  }
+
+  button {
+    width: 50%;
+    border-radius: 5rem;
+    margin: 0.5rem auto;
+    padding: 0.8rem;
+    border: none;
+  }
+
+  @media screen and (min-width: 600px) {
+    display: flex;
+
+    > img,
+    > div {
+      flex-basis: 50%;
+    }
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,20 @@
 <template>
   <category-block />
   <product-market-buttons />
+  <pay-wall />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import CategoryBlock from "@/components/layouts/main/blocks/CategoryBlock.vue";
 import ProductMarketButtons from "@/components/layouts/main/blocks/ProductMarket.vue";
+import PayWall from "@/components/layouts/main/blocks/PayWall.vue";
 
 @Options({
   components: {
     CategoryBlock,
     ProductMarketButtons,
+    PayWall,
   },
 })
 export default class HomeView extends Vue {}
